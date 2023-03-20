aedf7e1f9f2889c93a8378e91bfa704f0573d994
CrossNox <ijmermet@gmail.com>
2023-03-20 05:19:22 -0300

    commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from nes_py.wrappers import JoypadSpace
 
 from arkanoid import Arkanoid
 
-actions = [["NOOP"], ["left", "A"], ["right", "A"], ["A"]]
+actions = [["NOOP"], ["left"], ["right"], ["A"]]
 
 ark = Arkanoid()
 ark = JoypadSpace(ark, actions)
@@ -14,13 +14,19 @@ import numpy as np
 
 done = True
 action = 0
+shot_laser = 1
 for i in range(50_000):
     if done:
         _ = ark.reset()
         done = False
 
     state, reward, done, info = ark.step(action)
-    if info["ball_grid_y"] * (168 / 11) + 16 < info["vaus_left_x"]:
+    if info["vaus_status"] == "laser":
+        shot_laser += 1
+
+    if shot_laser % 2 == 0 and info["vaus_status"] == "laser":
+        action = 3
+    elif info["ball_grid_y"] * (168 / 11) + 16 < info["vaus_left_x"]:
         action = 1
     elif (
         info["ball_grid_y"] * (168 / 11)
@@ -32,8 +38,8 @@ for i in range(50_000):
         action = 2
     else:
         action = ark.action_space.sample(mask=np.array([1, 0, 0, 1], dtype=np.int8))
-    # pprint(info)
 
+    # pprint(info)
     ark.render()
     time.sleep(0.001)
 ark.close()
