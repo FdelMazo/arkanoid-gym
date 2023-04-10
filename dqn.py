adf4b10403e1a585c14fbd0414aadd95fda46615
CrossNox <ijmermet@gmail.com>
2023-04-10 02:03:37 -0300

    dqn skeleton

---
+++ dqn.py
@@ -0,0 +1,9 @@
+class DQNAgent:
+    def __init__(self, env):
+        self.env = env
+
+    def get_action(self, _screen, _info):
+        pass
+
+    def update(self, screen, info, action, reward, done, next_screen, next_info):
+        pass
