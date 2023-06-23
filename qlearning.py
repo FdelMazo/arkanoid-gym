58c33f42b5ba294ca3bf5338532b7abb3938de0d
Federico del Mazo <FdelMazo@fi.uba.ar>
2023-06-23 08:43:52 -0300

    Make learning rate decay with an exponential

--- qlearning.py
+++ qlearning.py
@@ -18,25 +18,38 @@ class QLearningAgent(ArkAgent):
     def __init__(
         self,
         env,
-        learning_rate: float = 0.1,
         discount_factor: float = 0.9,
+        learning_rate_start: float = 0.1,
+        learning_rate_end: float = 0.001,
+        learning_rate_decay: float = 50000,
         exploration_rate_start: float = 0.95,
         exploration_rate_end: float = 0.05,
         exploration_rate_decay: float = 50000,
     ):
         self.env = env
         self.steps = 0
-        self.learning_rate = learning_rate
         self.discount_factor = discount_factor
+
+        self.learning_rate_start = learning_rate_start
+        self.learning_rate_end = learning_rate_end
+        self.learning_rate_decay = learning_rate_decay
+        self.learning_rate = learning_rate_start
+
         self.exploration_rate_start = exploration_rate_start
         self.exploration_rate_end = exploration_rate_end
         self.exploration_rate_decay = exploration_rate_decay
         self.exploration_rate = exploration_rate_start
+
         # We avoid the "A" button
         self.q_table = defaultdict(lambda: np.zeros(env.action_space.n - 1))
 
     def get_action(self, _screen, info):
         self.steps += 1
+
+        self.learning_rate = self.learning_rate_end + (
+            self.learning_rate_start - self.learning_rate_end
+        ) * np.exp(-1.0 * self.steps / self.learning_rate_decay)
+
         self.exploration_rate = self.exploration_rate_end + (
             self.exploration_rate_start - self.exploration_rate_end
         ) * np.exp(-1.0 * self.steps / self.exploration_rate_decay)
